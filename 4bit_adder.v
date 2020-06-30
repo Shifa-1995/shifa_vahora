541db925916a71b1d2d49a978524faaf8622f592
Shifa-1995 <67653523+Shifa-1995@users.noreply.github.com>
2020-06-30 16:45:00 -0500

    Add files via upload

--- 4bit_adder.v
+++ 4bit_adder.v
@@ -1,20 +1,20 @@
-//structural modeling og 4 bit adder
+//structural modeling of 4 bit adder using adder
 //author : shifa vahora
-module bit_adder (s,cout,a,b,cin);
-input [3:0] a,b;
-input cin;
-output cout;
-output [3:0] s;
+module bit_adder (s,cout,a,b,cin);    //structural modeling of 4-bit adder
+input [3:0] a,b;            //inputs
+input cin;                  //inputs
+output cout;                //output carry
+output [3:0] s;             //output sum
 wire c1,c2,c3;
-fulladder FA1(s[0], c1, a[0], b[0], cin);
+fulladder FA1(s[0], c1, a[0], b[0], cin);       //instatiating 4 fulladder     
 fulladder FA2(s[1], c2, a[1], b[1], c1);
 fulladder FA3(s[2], c3, a[2], b[2], c2);
 fulladder FA4(s[3], cout, a[3], b[3], c3);
 endmodule
-module fullader (s,cout,a,b,c);
-input a,b,c;
-output s,cout;
-wire s1,c1,c2;
+module fullader (s,cout,a,b,c);     //behavioural modeling of fulladder
+input a,b,c;              //inputs of fulladder
+output s,cout;            //outputs of fulladder
+wire s1,c1,c2;             //interconnection
 xor GT1 (s1,a,b), GT2(s,s1,c), GT3(cout,c1,c2);
 and GT4 (c1,a,b), GT5(c2,s1,c);
 endmodule
--- 4bit_adder_tb.v
+++ 4bit_adder_tb.v
@@ -1,16 +1,16 @@
 //structural modeling of 4-bit adder(testbench)
 //author : shifa vahora
 module addertest;
-reg [3:0]a,b;
+reg [3:0]a,b;             //inputs
 reg c1;
-wire [3:0] s;
+wire [3:0] s;             //outputs
 wire c;
-fullader ADD(s,c,a,b,c1);
-initial 
+fullader ADD(s,c,a,b,c1);    //module instantiations
+initial                      //procedural block start
 begin
-$monitor($time,"s=%b,c=%b, a=%b, b=%b, c1=%b",s,c,a,b,c1);
+$monitor($time,"s=%b,c=%b, a=%b, b=%b, c1=%b",s,c,a,b,c1);   //monitoring inputs and outputs
 #5 a=4'b0000; 
-   b=4'b0001; 
+   b=4'b0001;                 //inputs given for testing
    c1=1'b1;
 #5 a=4'b1000; 
    b=4'b0001;
@@ -21,6 +21,6 @@ $monitor($time,"s=%b,c=%b, a=%b, b=%b, c1=%b",s,c,a,b,c1);
 #5 a=4'b1111; 
    b=4'b0001;
    c1=1'b0; 
-#5 $finish;
+#5 $finish;                      //finish the operation
 end
 endmodule
